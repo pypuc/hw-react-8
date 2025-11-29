5889e3b5d9be849b6d7b3539ec4a77e95bd91405
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-11-29 16:48:13 +0200

    work

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import Filter from "./components/Filter";
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
 
     const isExist = contacts.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
     );
 
     if (isExist) {
@@ -34,16 +34,16 @@ class App extends Component {
       number,
     };
 
-    this.setState(prev => ({
+    this.setState((prev) => ({
       contacts: [...prev.contacts, newContact],
     }));
 
     return true;
   };
 
-  deleteContact = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id)
+  deleteContact = (id) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !== id),
     }));
   };
 
@@ -58,7 +58,7 @@ class App extends Component {
 
     const normalized = filter.toLowerCase();
 
-    return contacts.filter(contact =>
+    return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(normalized)
     );
   };
@@ -72,10 +72,7 @@ class App extends Component {
 
         <h2>Contacts</h2>
 
-        <Filter
-          value={this.state.filter}
-          onChange={this.handleFilterChange}
-        />
+        <Filter value={this.state.filter} onChange={this.handleFilterChange} />
 
         <ContactList
           contacts={this.getFilteredContacts()}
